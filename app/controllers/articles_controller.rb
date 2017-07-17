3e60020d3377c10b1e8b10a73017fe9fcdd269c4
Benjamin Leuzinger <beleuz@gmx.ch>
2017-07-17 14:23:29 +0200

    Validations and Error Messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,18 +12,23 @@ class ArticlesController < ApplicationController
 
 
   def new
+    @article = Article.new
   end
 
 
   def create
       @article = Article.new(article_params)
 
-      @article.save
+      if @article.save
       redirect_to @article
-    end
-    private
-    def article_params
-      params.require(:article).permit(:title, :text)
+      else
+        render 'new'
+      end
 
-  end
 end
+  private
+      def article_params
+        params.require(:article).permit(:title, :text)
+
+      end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  root 'welcome#index'
+  root 'articles#index'
 
 end
