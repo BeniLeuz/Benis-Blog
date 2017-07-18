4fbd4de48db263a17fa524d0a8ce327e29335db9
Benjamin Leuzinger <beleuz@gmx.ch>
2017-07-18 11:31:39 +0200

    Creating Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments, only:  [:create]
+  end
 
   root 'welcome#index'
 
-end
+
+
+  end
+
+
+
+
